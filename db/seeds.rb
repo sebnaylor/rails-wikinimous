b12997ec601b8c417183a314ae4521e44148e17c
Seb Naylor <sebnaylor@gmail.com>
2022-02-16 08:54:58 +0000

    Made and seeded a db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+100.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.sentences
+  )
+  article.save
+end
